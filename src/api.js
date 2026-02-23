34480063f8b00a0679f4b85ff5f6f4655ed70b65
WSchlesner <75749271+WSchlesner@users.noreply.github.com>
2026-02-23 11:11:41 -0500

    QueuePriority Updates

--- src/api.js
+++ src/api.js
@@ -1,7 +1,42 @@
 const { CFToolsClientBuilder } = require("cftools-sdk");
 const { cftools } = require("../config.json");
 
-module.exports = new CFToolsClientBuilder()
+// ORIGINAL LINE module.exports = new CFToolsClientBuilder()
+const client = new CFToolsClientBuilder()
   .withServerApiId(cftools.serverId)
   .withCredentials(cftools.appId, cftools.appSecret)
   .build();
+
+// Extend the client with queue priority methods
+const api = {
+  // Base client methods
+  ...client,
+
+  // Get all queue priority entries
+  async getQueuePriority() {
+    const response = await client.get(`/v1/server/${cftools.serverId}/queuepriority`);
+    return response.data;
+  },
+
+  // Add or update a queue priority entry
+  async putQueuePriority({ cftools_id, comment, expires_at = null }) {
+    const response = await client.post(`/v1/server/${cftools.serverId}/queuepriority`, {
+      cftools_id: cftools_id.toString(),
+      comment,
+      expires_at
+    });
+    return response.data;
+  },
+
+  // Delete a queue priority entry
+  async deleteQueuePriority({ cftools_id }) {
+    const response = await client.delete(`/v1/server/${cftools.serverId}/queuepriority`, {
+      data: {
+        cftools_id: cftools_id.toString()
+      }
+    });
+    return response.data;
+  }
+};
+
+module.exports = api;
---
+++ src/commands/deletequeuepriority.js
@@ -0,0 +1,58 @@
+const { Command } = require("@sapphire/framework");
+const { SteamId64 } = require("cftools-sdk");
+const api = require("../api.js");
+const config = require("../../config.json");
+
+module.exports = class extends Command {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: "deletequeuepriority",
+      description: "Remove queue priority from a player",
+    });
+  }
+
+  /**
+   * @param {Command.Registry} registry
+   */
+  async registerApplicationCommands(registry) {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .setDMPermission(false)
+        .addStringOption((option) =>
+          option
+            .setName("steam64id")
+            .setDescription("ID of the player to remove queue priority from")
+            .setRequired(true)
+        )
+    );
+  }
+
+  /**
+   * @param {Command.ChatInputInteraction} interaction
+   */
+  async chatInputRun(interaction) {
+    if (!interaction.member.roles.cache.has(config.moderatorRole))
+      return interaction.reply(
+        "You don't have permission to use this command."
+      );
+    
+    await interaction.deferReply();
+    const steam64id = interaction.options.getString("steam64id", true);
+
+    try {
+      await api.deleteQueuePriority({
+        cftools_id: SteamId64.of(steam64id)
+      });
+    } catch (e) {
+      if (e?.message.startsWith("ResourceNotFound")) {
+        return await interaction.followUp("Player not found or has no queue priority.");
+      }
+      return await interaction.followUp(`Error: ${e.message}`);
+    }
+
+    await interaction.followUp(`Removed queue priority from \`${steam64id}\`.`);
+  }
+};
